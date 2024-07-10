9032c785b90c18a6a23bd5aadece3569bf3f5881
Personal <pavithara8300@gmail.com>
2024-07-10 23:06:16 +0530

    Project updated

--- tinder_app/main.py
+++ tinder_app/main.py
@@ -37,7 +37,7 @@ time.sleep(3)
 fb_login = driver.find_element(By.XPATH, value='//*[@id="u1146625330"]/div/div/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button')
 fb_login.click()
 
-#Switch to Facebook login window
+# Switch to Facebook login window
 time.sleep(2)
 base_window = driver.window_handles[0]
 fb_login_window = driver.window_handles[1]
@@ -47,48 +47,47 @@ print(driver.title)
 time.sleep(5)
 email = driver.find_element(By.XPATH, value='//*[@id="email"]')
 email.send_keys(EMAIL)
-password = driver.find_element(By.XPATH, value='//*[@id="pass"]')
-password.send_keys(PASSWORD)
+passwords = driver.find_element(By.XPATH, value='//*[@id="pass"]')
+passwords.send_keys(PASSWORD)
 email.send_keys(Keys.ENTER)
 
-#Switch back to Tinder window
+# Switch back to Tinder window
 driver.switch_to.window(base_window)
 print(driver.title)
 
-#Delay by 5 seconds to allow page to load.
-time.sleep(5)
+# Delay by 5 seconds to allow page to load.
+time.sleep(10)
 
-#Allow location
+# Allow location
 allow_location_button = driver.find_element(By.XPATH, value='/html/body/div[2]/div/div/div/div/div[3]/button[1]')
 allow_location_button.click()
 
-#Disallow notifications
+# Disallow notifications
 notifications_button = driver.find_element(By.XPATH, value='//*[@id="u1146625330"]/div/div/div/div/div[3]/button[1]')
 notifications_button.click()
 
-#Allow cookies
-cookies = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div[2]/div/div/div[1]/button')
-cookies.click()
+# Allow cookies
+# cookies = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+# cookies.click()
+time.sleep(10)
 
-
-#Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
+# Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
 for n in range(100):
 
-    #Add a 1 second delay between likes.
-    time.sleep(1)
+    # Add a 1 second delay between likes.
+    time.sleep(2)
 
     try:
         print("called")
-        like_button = driver.find_element(By.XPATH, value=
-            '//*[@id="u-1419960890"]/div/div[1]/div/div/main/div/div/div[1]/div/div[3]/div/div[4]/button')
+        like_button = driver.find_element(By.XPATH, value='//button[@class="button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a"]')
         like_button.click()
 
-    #Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
+    # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
     except ElementClickInterceptedException:
         try:
             match_popup = driver.find_element(By.CSS_SELECTOR, value=".itsAMatch a")
             match_popup.click()
 
-        #Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
+        # Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
         except NoSuchElementException:
             time.sleep(2)
